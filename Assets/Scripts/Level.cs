3b871b771ded21a9b008ad13918de17ca7dd2177
Nemanja Milenkovic <nemanjamilenkovic@live.com>
2021-04-02 02:42:58 +0200

    Implementing additional scenes
    - Add TextMeshPro to project
    - Implement a script used for changing levels
    - Create Menu Scene
    - Start implementing menu scene canvas
    - Create prefabs for objects that didn't have it

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
